e4c8f138f063e4ae1bbf090c69face404c8102cf
Denison <denison.el.p@gmail.com>
2025-05-27 19:12:02 -0300

    Refactor logic in solution for problem 3242 - Cobranças to improve readability and efficiency

--- 6445-programacao-dinamica-II/3242-cobrancas/3242-cobrancas.cpp
+++ 6445-programacao-dinamica-II/3242-cobrancas/3242-cobrancas.cpp
@@ -21,19 +21,14 @@ int calcular_valor_max(vector<ty_cliente> &clientes, int num_clientes, int dista
     int maior_valor_encontrado = 0;
 
     for(int i = 1; i <= num_clientes; i++)
-    {
         for(int j = distancia_max; j >= clientes.at(i - 1).distancia; j--)
-        {
             if(memo.at(j - clientes.at(i - 1).distancia).menor_indice >= 0
             && memo.at(j).valor_maximo < (memo.at(j - clientes.at(i - 1).distancia).valor_maximo + clientes.at(i - 1).valor_devido))
                 {
                     memo.at(j).menor_indice = i;
                     memo.at(j).valor_maximo = memo.at(j - clientes.at(i - 1).distancia).valor_maximo + clientes.at(i - 1).valor_devido;
-
                     maior_valor_encontrado = max(maior_valor_encontrado, memo.at(j).valor_maximo);
                 }
-        }
-    }
 
     return maior_valor_encontrado;
 }
